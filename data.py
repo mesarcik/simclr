850604fb6898ed5c32ef2e8d42a489c804787eb3
Misha Mesarcik <m.mesarcik@uva.nl>
2021-12-08 17:28:19 +0100

    fist commit structure

---
+++ data.py
@@ -0,0 +1,1 @@
+# script to  load and augment data 
---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import torch
+from sklearn.metrics import roc_auc_score
+import torch.optim as optim
+import argparse
+from losses import CompactnessLoss, EWCLoss
+import utils
+from copy import deepcopy
+from tqdm import tqdm
